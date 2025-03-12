f41fe13f05e1fbce949357697895eafae70c3cbb
Jellyvous <phihoangdiep2506@gmail.com>
2025-03-12 16:21:48 +0700

    Update login register feature

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
-UPLOAD_FOLDER = 'uploads'
-RESULT_FOLDER = 'results'
-PRE_FOLDER = 'pre'
+import os
+
+# Paths
+UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'uploads/')
+RESULT_FOLDER = os.getenv('RESULT_FOLDER', 'results/')
+
+# MongoDB URI
+MONGO_URI = os.getenv('MONGO_URI')
+
+# Secret key for sessions
+SECRET_KEY = os.getenv('SECRET_KEY')
